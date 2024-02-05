4c8736cf21616ac85439111d47adc9d0651676c0
Andrei Zaharia <andrei.zaharia@tremend.com>
2024-02-05 15:26:28 +0200

    first commit

--- src/test/java/com/tremend/practice/course2/ExerciseHello.java
+++
@@ -1,9 +0,0 @@
-package com.tremend.practice.course2;
-
-public class ExerciseHello {
-        public static void main(String[] args) {
-
-                    System.out.println("Hello World!");
-
-                }
-            }
